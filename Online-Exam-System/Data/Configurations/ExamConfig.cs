a0e5e53458b3166773c78cee0352f34616a037de
Omar Alfarouk <141720964+OmarAlfar0uk@users.noreply.github.com>
2025-09-29 19:52:43 +0300

    add-migrations&&Configrations

---
+++ Online-Exam-System/Data/Configurations/ExamConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Online_Exam_System.Models;
+
+namespace Online_Exam_System.Data.Configurations
+{
+    public class ExamConfig : IEntityTypeConfiguration<Exam>
+    {
+        public void Configure(EntityTypeBuilder<Exam> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(e => e.PictureUrl)
+                 .IsRequired(false)
+                 .HasMaxLength(500);
+
+            builder.Property(e => e.StartDate)
+                 .HasConversion(
+                    v => v.ToDateTime(TimeOnly.MinValue),
+                    v => DateOnly.FromDateTime(v));
+
+            builder.Property(e => e.EndDate)
+                 .HasConversion(
+                    v => v.ToDateTime(TimeOnly.MinValue),
+                    v => DateOnly.FromDateTime(v));
+
+            builder.Property(e => e.duration)
+                .HasConversion(
+                    v => v.ToTimeSpan(),
+                    v => TimeOnly.FromTimeSpan(v));
+
+        }
+    }
+}
--- Online-Exam-System/Models/Exam.cs
+++ Online-Exam-System/Models/Exam.cs
@@ -3,7 +3,7 @@
     public class Exam : BaseEntity
     {
         public required string Title { set; get; }
-        public string PictureUrl { get; set; } = default!;
+        public string? PictureUrl { get; set; } = default!;
         public DateOnly StartDate { get; set; }
         public DateOnly EndDate { get; set; }
         public TimeOnly duration { get; set; }
