f90670071e5aefcb1994a34230616dcddf169216
Omar Alfarouk <141720964+OmarAlfar0uk@users.noreply.github.com>
2025-10-02 18:11:45 +0300

    finsh-Pagination-search -Filtration -cahing and endpoint GetAllExam

---
+++ Online-Exam-System/Features/Exam/GetAll/GetAllExamEndPoint.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Online_Exam_System.Shared;
+
+namespace Online_Exam_System.Features.Exam.GetAll
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GetAllExamEndPoint : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public GetAllExamEndPoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll([FromQuery] ExamQueryParameters parameters)
+        {
+            var query = new GetAllExamQuery(parameters);
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
---
+++ Online-Exam-System/Features/Exam/GetAll/GetAllExamQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Online_Exam_System.Shared;
+
+namespace Online_Exam_System.Features.Exam.GetAll
+{
+    public record GetAllExamQuery(ExamQueryParameters Parameters) : IRequest<PagedResult<GetAllExamsDTOs>>;
+       
+}
