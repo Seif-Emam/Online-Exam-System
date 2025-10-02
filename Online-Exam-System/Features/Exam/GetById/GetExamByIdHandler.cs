a2ceb6983308c19c34ccd84cafefad5bc1467770
Omar Alfarouk <141720964+OmarAlfar0uk@users.noreply.github.com>
2025-10-02 23:28:39 +0300

    Finsh GetExamById

---
+++ Online-Exam-System/Features/Exam/GetById/GetExamByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Online_Exam_System.Contarcts;
+using Online_Exam_System.Models;
+
+namespace Online_Exam_System.Features.Exam.GetById
+{
+    public class GetExamByIdHandler(IUnitOfWork _unitOfWork) : IRequestHandler<GetExamByIdQuerey, GetExamsByIdDTOs>
+    {
+        public async Task<GetExamsByIdDTOs> Handle(GetExamByIdQuerey request, CancellationToken cancellationToken)
+        {
+            var exam = await _unitOfWork.GetRepository<Models.Exam>().GetByIdAsync(request.Id);
+            if (exam == null)
+                return null!;
+            return new GetExamsByIdDTOs
+            {
+               Id = exam.Id,
+               Title = exam.Title,
+               PictureUrl = exam.PictureUrl,
+               StartDate = exam.StartDate,
+               EndDate = exam.EndDate,
+               Duration = exam.duration
+
+
+            };
+
+        }
+    }
+}
---
+++ Online-Exam-System/Features/Exam/GetById/GetExamsByIdDTOs.cs
@@ -0,0 +1,12 @@
+﻿namespace Online_Exam_System.Features.Exam.GetById
+{
+    public class GetExamsByIdDTOs
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? PictureUrl { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public TimeOnly Duration { get; set; }
+    }
+}
