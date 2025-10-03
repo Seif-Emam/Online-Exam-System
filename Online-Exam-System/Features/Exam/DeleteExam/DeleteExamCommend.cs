cbc7d12d0f370fffb5e635739183ea4d43034d3b
Omar Alfarouk <141720964+OmarAlfar0uk@users.noreply.github.com>
2025-10-03 12:37:46 +0300

    feat(Exam): add try-catch in DeleteExamHandler for safe exam deletion
    - Added null check for exam before deleting
    - Wrapped delete logic in try-catch to handle unexpected exceptions
    - Return false if exam not found or exception occurs

---
+++ Online-Exam-System/Features/Exam/DeleteExam/DeleteExamCommend.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace Online_Exam_System.Features.Exam.DeleteExam
+{
+    public record DeleteExamCommend(Guid id) : IRequest<bool>;
+  
+}
---
+++ Online-Exam-System/Features/Exam/DeleteExam/DeleteExamOrchestrator.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Online_Exam_System.Contarcts;
+using Online_Exam_System.Features.Exam.GetById;
+using Online_Exam_System.Features.Exam.UpdateExam;
+using Online_Exam_System.Services;
+
+namespace Online_Exam_System.Features.Exam.DeleteExam
+{
+    public   class DeleteExamOrchestrator(IMediator mediator, IImageHelper imageHelper) : IDeleteExamCommandOrchestrator
+    {
+        public async Task<bool> DeleteExamAsync(Guid id)
+        {
+            var Exam = await mediator.Send(new GetExamByIdQuerey(id));
+            if (Exam == null)
+                return false;
+
+            var isDeleted = await mediator.Send(new DeleteExamCommend(id));
+            if (isDeleted)
+                imageHelper.DeleteImage(Exam.PictureUrl);
+
+            return isDeleted;
+        }
+    }
+}
