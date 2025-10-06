7015164617e7027c0c88ac56b499048babe2212e
Omar Alfarouk <141720964+OmarAlfar0uk@users.noreply.github.com>
2025-10-06 22:53:20 +0300

    Created Models FRom Questions

---
+++ Online-Exam-System/Models/Questions/Answer.cs
@@ -0,0 +1,13 @@
+﻿namespace Online_Exam_System.Models.Questions
+{
+    public class Answer : BaseEntity
+    {
+        public int QuestionId { get; set; }
+        public Question Question { get; set; } = default!;
+
+        public int ChoiceId { get; set; }
+        public Choice Choice { get; set; } = default!;
+
+        public string UserId { get; set; } = default!;
+    }
+}
